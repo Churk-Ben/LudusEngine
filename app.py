c21fc21bf4cecec1970c1852dd26a4803ccfe7ef
churk_ben <2779675416@qq.com>
2025-12-04 23:16:54 +0800

    feat: 实现窗口化运行

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from src.Server import app, socketio
+from threading import Thread
+import os
+
+
+def open_app():
+    if not os.environ.get("WERKZEUG_RUN_MAIN"):
+        os.system('start "" msedge.exe --app="http://localhost:5000"')
+
 
 if __name__ == "__main__":
+    Thread(target=open_app).start()
     socketio.run(app, host="0.0.0.0", port=5000, debug=True)
