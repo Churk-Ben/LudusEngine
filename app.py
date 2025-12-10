966c75d76635e5f3d52c77a42c8f136be8303afe
Churk <2779675416@qq.com>
2025-12-10 13:04:35 +0800

    update

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
+from ssl import socket_error
 from src.Server import app, socketio
 from src.Logger import get_logger
 from src.Config import load_config
 import webbrowser
 import platform
 import os
+import sys
+import webview
+import threading
 
 log = get_logger("APP")
 log.info("-" * 80)
-log.info(f"操作系统 (System): {platform.system()}")
-log.info(f"操作系统版本 (Release): {platform.release()}")
-log.info(f"详细版本 (Version): {platform.version()}")
-log.info(f"硬件架构 (Machine): {platform.machine()}")
-log.info(f"处理器 (Processor): {platform.processor()}")
+log.info(f"操作系统 - System: {platform.system()}")
+log.info(f"操作系统版本 - Release: {platform.release()}")
+log.info(f"详细版本 - Version: {platform.version()}")
+log.info(f"硬件架构 - Machine: {platform.machine()}")
+log.info(f"处理器 - Processor: {platform.processor()}")
 log.info("-" * 80)
-log.info(f"平台信息汇总 (Platform): {platform.platform()}")
+log.info(f"平台信息汇总 - Platform: {platform.platform()}")
 
 
 def open_browser_in_app_mode(url):
@@ -35,8 +39,19 @@ def open_browser_in_app_mode(url):
         log.info(f"非 Windows 系统, 直接打开浏览器.")
         webbrowser.open_new(url)
 
+def open_app_in_webview():
+    # 1. 后台线程跑 Flask-SocketIO
+    t = threading.Thread(target=lambda: socketio.run(app,host="127.0.0.1",port=5000,debug=False), daemon=True)
+    t.start()
 
-def main():
+    # 2. 前台的 pywebview 窗口
+    webview.create_window('我的桌面应用',
+                          url='http://localhost:5000',
+                          width=1200, height=800,
+                          resizable=True)
+    webview.start()
+
+def browser_main():
     # 加载配置
     config = load_config()
     log.info(f"配置加载完成: {config}")
@@ -61,4 +76,9 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # 打包后资源路径兼容
+    # def base_path(*p):
+    #     return os.path.join(getattr(sys, '_MEIPASS', os.path.abspath('.')), *p)
+    browser_main()
+
+
